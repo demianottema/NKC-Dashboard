9b3e8c768d1ad41acdb7181b3598a6effc75a684
demianottema <44736169+demianottema@users.noreply.github.com>
2025-07-09 11:51:33 +0200

    Added builder for forms

--- app/Filament/Resources/Groups/Schemas/GroupForm.php
+++ app/Filament/Resources/Groups/Schemas/GroupForm.php
@@ -6,13 +6,20 @@ use App\Models\Section;
 use App\Models\SectionPage;
 use Filament\Schemas\Schema;
 use Filament\Schemas\JsContent;
+use Filament\Forms\Components\Select;
 use Filament\Schemas\Components\Tabs;
+use Filament\Forms\Components\Builder;
 use Filament\Schemas\Components\Group;
+use Filament\Forms\Components\Repeater;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Schemas\Components\FusedGroup;
+use Filament\Forms\Components\Builder\Block;
 use Filament\Schemas\Components\Utilities\Get;
+use Filament\Forms\Components\Repeater\TableColumn;
 use App\Filament\Resources\Groups\SectionTypes\SectionPageType;
 
 class GroupForm
@@ -31,11 +38,58 @@ class GroupForm
                         foreach ($sectionPages as $sectionPage) {
                             $tabs[] = Tab::make($sectionPage->name)
                                 ->statePath($sectionPage->id)
-                                ->schema(array_merge([
+                                ->schema([
                                     TextInput::make('section_page_name')
                                     ->required(),
+
+                                    Builder::make('content')
+                                        ->collapsible()
+                                        ->blockNumbers(false)
+                                        
+                                        ->blocks([
+                                            Block::make('Content')
+                                                
+                                                ->label(function (?array $state): string {
+                                                    if ($state === null) {
+                                                        return 'Content';
+                                                    }
+
+                                                    return $state['title'] ?? 'Titel';
+                                                })
+                                                ->schema([
+                                                    TextInput::make('title')
+                                                        ->live(onBlur: true)
+                                                        ->required(),
+                                                    RichEditor::make('content'),
+                                                        
+                                                ]),
+                                            Block::make('actionsSteps')
+                                                ->label('Actiepunten')
+                                                ->maxItems(1)
+                                                ->schema([
+                                                    Repeater::make('actionsSteps')
+                                                    ->table([
+                                                        TableColumn::make('Wie?'),
+                                                        TableColumn::make('Wat?'),
+                                                        TableColumn::make('Wanneer?'),
+                                                    ])
+                                                    ->schema([
+                                                        Select::make('users')
+                                                            // Should be enabled if https://github.com/filamentphp/filament/issues/16841 is fixed
+                                                            // ->multiple() 
+                                                            ->hiddenLabel()
+                                                            ->options(fn () => $sectionPage->section->group->users->pluck('name', 'id')->toArray())
+                                                            ->required(),
+
+                                                        TextInput::make('title')
+                                                            ->hiddenLabel(),
+                                                        
+                                                        DatePicker::make('deadline')
+                                                    ])
+                                                ]),
+                                        ])
                                     
-                                ], SectionPageType::renderSchema($sectionPage)));
+                                ]);
                         }
                         
                         return $tabs ?? [];
